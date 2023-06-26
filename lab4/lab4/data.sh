1e2add9e5c6a465f30aa851ff5490b223820bd25
LajkoniQ <137609905+LajkoniQ@users.noreply.github.com>
2023-06-26 15:52:02 +0200

    Delete lab4/lab4 directory

--- lab4/lab4/data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Dzisiejsza data: $(date)"
-
-exit 0
--- lab4/lab4/skrypt3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Dostępne opcje:"
-echo "-h, --help            Wyświetla pomoc i dostępne opcje"
-echo "--date                Wyświetla dzisiejszą datę"
-echo "--logs [liczba]       Tworzy automatycznie [liczba] plików logx.txt, gdzie x to numer pliku od 1 do [liczba]"
-echo "                      W każdym pliku zawarta jest nazwa, nazwa skryptu i data"
-
-exit 0
